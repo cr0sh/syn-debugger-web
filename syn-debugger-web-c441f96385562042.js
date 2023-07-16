93ba80942081353458496350e970073ed3f5a175
cr0sh <cr0sh@users.noreply.github.com>
2023-07-16 11:21:34 +0000

    deploy: 0260f3cbe8df041b1a4e0692353836652135b6d6

--- syn-debugger-web-c441f96385562042.js
+++ syn-debugger-web-c441f96385562042.js
@@ -646,7 +646,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('syn-debugger-web-e1c46c2025a3c4b4_bg.wasm', import.meta.url);
+        input = new URL('syn-debugger-web-c441f96385562042_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
