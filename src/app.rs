49fdb5c148fad478d7fc8a8de4a12a3ebbe51f25
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-07-16 23:54:11 +0900

    fix: font size on desktop(again)

--- src/app.rs
+++ src/app.rs
@@ -70,7 +70,7 @@ pub fn app() -> Html {
         <main>
             <div class={ classes!("flex", "flex-col", "items-stretch", "px-20", "py-10", "h-screen") }>
                 <h1 class={ classes!("py-5", "text-2xl") }>{ format!("syn-debugger-web v{}", env!("CARGO_PKG_VERSION")) }</h1>
-                <div class={ classes!("flex", "text-xl", "lg:text-sm", "flex-col", "lg:flex-row", "grow", "w-full", "h-4/5") }>
+                <div class={ classes!("flex", "text-xl", "flex-col", "lg:flex-row", "grow", "w-full", "h-4/5") }>
                     <div class={ classes!("flex", "flex-col", "px-5", "text-md", "w-full", "lg:w-1/2", "h-1/2", "lg:h-full") }>
                         <Input {input_ref} onchange={onchange.clone()}/>
                     </div>
@@ -107,6 +107,7 @@ fn input(props: &InputProperties) -> Html {
                     "flex-1",
                     "px-2",
                     "pt-2",
+                    "lg:text-sm",
                     "text-justify",
                     "text-start",
                     "font-mono",
@@ -164,6 +165,7 @@ fn output(props: &OutputProperties) -> Html {
                     "flex-1",
                     "px-2",
                     "pt-2",
+                    "lg:text-sm",
                     "text-justify",
                     "text-start",
                     "font-mono",
